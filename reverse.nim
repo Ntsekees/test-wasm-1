34f07d2599200166c9025fe29821af8623142af8
Ntsekees <ntsekees@users.noreply.github.com>
2023-01-30 21:25:30 +0100

    Adding a few missing semicolons.

--- reverse.nim
+++ reverse.nim
@@ -1,6 +1,7 @@
 
 proc reverse*(plaintext: var cstring, length: cint): void {.exportc.} =
   var i: cint = 0
+  #return cast[int](plaintext[0])
   #plaintext[0] = '@'
   while i < (length / 2).cint:
     let value: char = plaintext[i];
