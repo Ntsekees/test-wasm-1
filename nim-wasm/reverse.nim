752fc92cf50c848c161b99980d71a6f2d6251193
Ntsekees <ntsekees@users.noreply.github.com>
2023-02-04 23:57:55 +0100

    Adding explanatory comments to ⟦nim-wasm/reverse.nim⟧.

--- nim-wasm/reverse.nim
+++ nim-wasm/reverse.nim
@@ -14,11 +14,17 @@ proc reverse(plaintext: var cstring, length: cint): void =
   return
 
 proc wa_reverse*(): void {.exportc.} =
+  # First, we ask Javascript to communicate the size of the input string:
   let length = wa_input_size()
+  # Then we create a buffer of the appropriate capacity:
   var cs = newStringOfCap(length).cstring
+  # Now we ask Javascript to write the input in the buffer:
   wa_input(cs)
+  # Now that we have the input string, we can apply the reverse function on it:
   reverse(cs, length.cint)
+  # Now, we tell Javascript to read the reversed string and save it to a safe place (so that it isn't lost to garbage collection):
   wa_output(cs, length)
+  # This is inessential, but for the sake of demonstration, we also send the string we produced to Javascript's “console.log” function, for printing the string to the web console.
   js_log(cs, length)
   return
 
